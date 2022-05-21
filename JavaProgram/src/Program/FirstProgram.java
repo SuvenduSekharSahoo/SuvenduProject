04f574c72b95e49540c80081d1f87d07866104a4
Dell <Dell@DESKTOP-B2UQ05A>
2022-05-21 19:53:41 +0530

    new

--- JavaProgram/src/Program/FirstProgram.java
+++ JavaProgram/src/Program/FirstProgram.java
@@ -3,7 +3,8 @@ package Program;
 public class FirstProgram {
 	public static void main(String[]args)
 	{
-	System.out.println("hello");
+	 System.out.println("hello");
+	 System.out.println("Hello world");
 	}
 	}
 
